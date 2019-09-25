00fcd9cd2886293e1ce38972f0797971a3b1933c
openPyramid <openPyramid@163.com>
2019-09-25 15:58:38 +0800

    当Stop按键按下时，导航到睡眠质量页面去，建立此导航关系

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -18,6 +18,7 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
@@ -44,6 +45,14 @@ class SleepTrackerViewModel(
     private val nights = database.getAllNights()
     val nightsSting = Transformations.map(nights) { nights ->
         formatNights(nights, application.resources)
+    }
+
+    private var _navigateToSleepQuality = MutableLiveData<SleepNight>()
+    val navigateToSleepQuality : LiveData<SleepNight>
+        get() = _navigateToSleepQuality
+
+    fun doneNavigating() {
+        _navigateToSleepQuality.value = null
     }
 
     init {
@@ -99,6 +108,7 @@ class SleepTrackerViewModel(
             val oldNight = tonight.value ?: return@launch
             oldNight.endTimeMilli = System.currentTimeMillis()
             updata(oldNight)
+            _navigateToSleepQuality.value = oldNight
         }
     }
 
